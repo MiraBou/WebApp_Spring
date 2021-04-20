c13857ef70959c2c860fab9e17f2e19b924d18e1
MIRA B <>
2021-04-20 16:12:59 +0000

    changesUsers

--- src/main/java/com/demo/security/SecurityConfig.java
+++ src/main/java/com/demo/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.authentication.builders.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,16 +8,25 @@ import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 //@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+@Autowired
+private DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder passwordEncoder= passwordEncoder();
         //super.configure(auth);
         //auth.inMemoryAuthentication().withUser("mira").password("{noop}mira").roles("USER");
-        auth.inMemoryAuthentication().withUser("user").password(passwordEncoder.encode("mira")).roles("USER");
+        //System.out.println("******************************"+passwordEncoder.encode("1234"));
+        //System.out.println("******************************"+passwordEncoder.encode("mira"))
+        auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery("select username as principal,password as credentials,active from user where username=?")
+                .authoritiesByUsernameQuery("select username as principal,role as role from users_roles where username=?")
+        .passwordEncoder(passwordEncoder)
+        .rolePrefix("ROLE_");
+       // auth.inMemoryAuthentication().withUser("user").password(passwordEncoder.encode("mira")).roles("USER");
         auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("mira")).roles("ADMIN");
     }
     @Override
